5c8c328f1c329d21b5235826ec66d88eee938fa0
Ekwa <linusrhoden@hotmail.se>
2024-12-10 12:26:54 +0100

    Boss wave v0.5

---
+++ Alucard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyMonoGame;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace EasyStart
+{
+    internal class Alucard : Actor
+    {
+        private float Speed = 500;
+        private Player player;
+        public Alucard(Player player)
+        {
+            this.player = player;
+        }
+        public override void Update(GameTime gameTime)
+        {
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            TurnTowards(player.X, player.Y);
+            Move(deltaTime * 200);
+        }
+    }
+}
--- MyWorld.cs
+++ MyWorld.cs
@@ -26,28 +26,39 @@ namespace EasyStart
 
         public override void Update(GameTime gameTime)
         {
-            // Display the current wave
-            ShowText("Wave: " + current_wave, 960, 50);
+           
 
             base.Update(gameTime);
 
-            // Make the time decrease each update
             time -= gameTime.ElapsedGameTime.TotalSeconds;
 
-            // Spawn new goblins with each wave
             if (time < 0 && current_wave < 10)
             {
-                time = 5;
-                current_wave += 1;
+                time = 5; 
+                current_wave++;
 
+                ShowText("Wave: " + current_wave, 960, 50);
+
+                Random random = new Random(); // Random generator
+
+               
                 for (int i = 0; i < current_wave; i++)
                 {
-                    Add(new Goblin(my_player), "rsz_goblin_idle", 500, 500);
+                    int randomX = random.Next(100, 1820); 
+                    int randomY = random.Next(100, 980);  
+                    Add(new Goblin(my_player), "rsz_goblin_idle", randomX, randomY);
                 }
             }
 
+            if (time > 0 && current_wave == 10)
+            {
+                current_wave++;
 
-
+                ShowText("BOSS WAVE!", 960, 50);
+                
+                Add(new Alucard(my_player), "alucard-removebg-preview", 100, 100);
+                
+            }
         }
 
     }
