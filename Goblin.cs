5b54eac74522fa4aee0b34f994a650d525a0f4de
TangViktor <Viktor.tang13@gmail.com>
2025-01-30 09:34:19 +0100

    Spawntimer on enemies more specifically the Goblins.

--- Goblin.cs
+++ Goblin.cs
@@ -7,22 +7,39 @@ using EasyMonoGame;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
-
 namespace EasyStart
 {
     internal class Goblin : Actor
     {
         private float Speed = 500;
         private Player player;
+        private float standStillTimer = 0f;
+        private bool isStandingStill = true;
+        private float standStillDuration = 1f;
+
         public Goblin(Player player)
         {
             this.player = player;
         }
+
         public override void Update(GameTime gameTime)
         {
             var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            TurnTowards(player.X, player.Y);
-            Move(deltaTime * 200);
+
+            if (isStandingStill)
+            {
+                standStillTimer += deltaTime;
+
+                if (standStillTimer >= standStillDuration)
+                {
+                    isStandingStill = false;
+                }
+            }
+            else
+            {
+                TurnTowards(player.X, player.Y);
+                Move(deltaTime * 200);
+            }
         }
     }
 }
